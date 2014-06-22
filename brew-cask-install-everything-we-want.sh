6f38968878c2806172438680336a22c2f759add3
SixArm <sixarm@sixarm.com>
2014-06-22 12:53:20 -0600

    Add notes on non-cask apps

--- brew-cask-install-everything-we-want.sh
+++ brew-cask-install-everything-we-want.sh
@@ -1,5 +1,8 @@
 ### Use brew cask to install typical Mac applications.
 
+# See notes at bottom about applications that this script doesn't cover.
+
+
 # Install Cask.
 # We must already have installed brew.
 #
@@ -10,6 +13,7 @@ brew install caskroom/cask/brew-cask
 #
 brew cask install iterm2
 brew cask install firefox
+brew cask install sublime-text
 
 # Install  applications alphabetically.
 # This list does *not* have the applications that
@@ -122,4 +126,53 @@ brew cask install google-hangouts
 brew cask install heroku-toolbelt
 brew cask install unity-web-player
 
+##
+# Applications we want that are not available via cask:
+#
+#   * 0xDBE EAP
+#   * Backblaze
+#   * Kiwix
+#   * Sublime Text 3
+#   * SoapUI-5.0.0.app
+#   * SoapUI-Tutorials
+#
+# Applications that come from Apple automatically:
+#
+#   * System: App Store, Apple Configurator, Launchpad, Mission Control, System Preferences
+#   * Calculator, Calendar, Chess, Contacts, Dashboard, Dictionary, DVD Player, FaceTime, Game Center,
+#     Mail, Messages, Notes,  Photo Booth, Preview, QuickTime Player, Reminders, Safari, TextEdit
+#   * iApps: iBooks, iCloud, iMovie, iPhoto, iTunes, iWork
+#   * Creativity: GarageBand, Keynote, Numbers, Pages 
+#
+# Applications with origin TBD:
+#
+#   * App for Facebook, Gmail.
+#   * Automator
+#   * Battery Health
+#   * Cloud
+#   * Coffitivity
+#   * Disk Diag
+#   * Docs for Xcode
+#   * DxO Perspective
+#   * Font Book
+#   * Fotor
+#   * Garmin *
+#   * Image Capture.app
+#   * MailTab for Gmail.app
+#   * Maps.app
+#   * Memory Clean.app
+#   * MenuBar Stats.app
+#   * Microsoft Remote Desktop.app
+#   * Mint QuickView.app
+#   * Prepo.app
+#   * Screen Recorder Robot Lite.app
+#   * Stickies.app
+#   * Tab for Reddit.app
+#   * The Unarchiver.app
+#   * Time Machine.app
+#   * Twitter.app
+#   * VirtualDJ
+#   * Wunderlist.app
+#   * Xcode.app
+#   * xScan.app
 
