d64c72b7fa74ff160cce0a130fa49a71e8f6ea6f
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-06-27 13:42:24 -0700

    Create homebrew-fish-completion.fish

---
+++ bin/homebrew-fish-completion.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+# Homebrew fish completion
+# https://docs.brew.sh/Shell-Completion
+
+if test -d (brew --prefix)"/share/fish/completions"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+end
+
+if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+end
