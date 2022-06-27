3798da28db327b926a61522085f0c4c026515308
Joel Parker Henderson <joel@joelparkerhenderson.com>
2022-06-27 13:41:35 -0700

    Create homebrew-bash-completetion.bash

---
+++ bin/homebrew-bash-completetion.bash
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Homebrew bash completion
+# https://docs.brew.sh/Shell-Completion
+
+if type brew &>/dev/null
+then
+  HOMEBREW_PREFIX="$(brew --prefix)"
+  if [[ -r "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh" ]]
+  then
+    source "${HOMEBREW_PREFIX}/etc/profile.d/bash_completion.sh"
+  else
+    for COMPLETION in "${HOMEBREW_PREFIX}/etc/bash_completion.d/"*
+    do
+      [[ -r "${COMPLETION}" ]] && source "${COMPLETION}"
+    done
+  fi
+fi
